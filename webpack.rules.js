266684d35ac7472c6ddbd46f68ed5cf28ccd91dc
Tibor Rogulja <tibor.rogulja@tiskarazagreb.hr>
2020-07-24 16:46:40 +0200

    url-loader

--- webpack.rules.js
+++ webpack.rules.js
@@ -14,6 +14,13 @@ module.exports = [
       },
     },
   },
+  {
+    test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+    loader: 'url-loader',
+    options: {
+      limit: 1000000,
+    },
+  },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
